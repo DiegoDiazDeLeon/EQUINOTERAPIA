2ea51d46f0548b7cbc4436774656ed65aaaf4b75
DiegoDiazDeLeon <diegodiazdeleoncai@gmail.com>
2022-09-26 02:35:50 -0500

    Mostrar Informacion de la base de Datos

--- EQUINOTERAPIA/Citas.Designer.cs
+++ EQUINOTERAPIA/Citas.Designer.cs
@@ -29,30 +29,33 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Citas));
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.ta1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label_CURP = new System.Windows.Forms.Label();
             this.button_Buscar = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView_Citas = new System.Windows.Forms.DataGridView();
             this.button_AgregarCita = new System.Windows.Forms.Button();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.tabControl1.SuspendLayout();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Edad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ta1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Citas)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Citas)).BeginInit();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // ta1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 12);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(776, 426);
-            this.tabControl1.TabIndex = 2;
+            this.ta1.Controls.Add(this.tabPage1);
+            this.ta1.Controls.Add(this.tabPage2);
+            this.ta1.Location = new System.Drawing.Point(12, 12);
+            this.ta1.Name = "ta1";
+            this.ta1.SelectedIndex = 0;
+            this.ta1.Size = new System.Drawing.Size(1159, 582);
+            this.ta1.TabIndex = 2;
             // 
             // tabPage1
             // 
@@ -65,16 +68,27 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(768, 400);
+            this.tabPage1.Size = new System.Drawing.Size(1151, 556);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // label_CURP
             // 
             this.label_CURP.AutoSize = true;
             this.label_CURP.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_CURP.Location = new System.Drawing.Point(178, 21);
+            this.label_CURP.Location = new System.Drawing.Point(127, 6);
             this.label_CURP.Name = "label_CURP";
             this.label_CURP.Size = new System.Drawing.Size(48, 16);
             this.label_CURP.TabIndex = 4;
@@ -83,7 +97,7 @@
             // button_Buscar
             // 
             this.button_Buscar.Image = ((System.Drawing.Image)(resources.GetObject("button_Buscar.Image")));
-            this.button_Buscar.Location = new System.Drawing.Point(359, 34);
+            this.button_Buscar.Location = new System.Drawing.Point(308, 6);
             this.button_Buscar.Name = "button_Buscar";
             this.button_Buscar.Size = new System.Drawing.Size(52, 41);
             this.button_Buscar.TabIndex = 3;
@@ -91,7 +105,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(181, 55);
+            this.textBox1.Location = new System.Drawing.Point(130, 23);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(172, 20);
             this.textBox1.TabIndex = 2;
@@ -99,9 +113,13 @@
             // dataGridView_Citas
             // 
             this.dataGridView_Citas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Citas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Apellido,
+            this.Edad});
             this.dataGridView_Citas.Location = new System.Drawing.Point(21, 81);
             this.dataGridView_Citas.Name = "dataGridView_Citas";
-            this.dataGridView_Citas.Size = new System.Drawing.Size(725, 290);
+            this.dataGridView_Citas.Size = new System.Drawing.Size(1114, 452);
             this.dataGridView_Citas.TabIndex = 1;
             // 
             // button_AgregarCita
@@ -111,54 +129,59 @@
             this.button_AgregarCita.ForeColor = System.Drawing.Color.Black;
             this.button_AgregarCita.Image = ((System.Drawing.Image)(resources.GetObject("button_AgregarCita.Image")));
             this.button_AgregarCita.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button_AgregarCita.Location = new System.Drawing.Point(21, 21);
+            this.button_AgregarCita.Location = new System.Drawing.Point(995, 23);
             this.button_AgregarCita.Name = "button_AgregarCita";
             this.button_AgregarCita.Size = new System.Drawing.Size(140, 54);
             this.button_AgregarCita.TabIndex = 0;
             this.button_AgregarCita.Text = "Agregar Cita";
             this.button_AgregarCita.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button_AgregarCita.UseVisualStyleBackColor = false;
+            this.button_AgregarCita.Click += new System.EventHandler(this.button_AgregarCita_Click);
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(768, 400);
+            this.tabPage2.Size = new System.Drawing.Size(1151, 556);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // pictureBox1
+            // Nombre
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(652, 6);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(94, 69);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Apellido
+            // 
+            this.Apellido.HeaderText = "Apellido";
+            this.Apellido.Name = "Apellido";
+            // 
+            // Edad
+            // 
+            this.Edad.HeaderText = "Edad";
+            this.Edad.Name = "Edad";
             // 
             // Citas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(1183, 606);
+            this.Controls.Add(this.ta1);
             this.Name = "Citas";
             this.Text = "Citas";
-            this.tabControl1.ResumeLayout(false);
+            this.ta1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Citas)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Citas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl ta1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.DataGridView dataGridView_Citas;
@@ -167,5 +190,8 @@
         private System.Windows.Forms.Button button_Buscar;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Edad;
     }
 }
--- EQUINOTERAPIA/Form1.Designer.cs
+++ EQUINOTERAPIA/Form1.Designer.cs
@@ -48,6 +48,8 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.labelFechaInicioCaballo = new System.Windows.Forms.Label();
             this.ActualizaBeneficiario = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // agregaUsuario
@@ -218,11 +220,33 @@
             this.ActualizaBeneficiario.UseVisualStyleBackColor = true;
             this.ActualizaBeneficiario.Click += new System.EventHandler(this.ActualizaBeneficiario_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(336, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 36);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Visualiza";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(378, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "User";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.ActualizaBeneficiario);
             this.Controls.Add(this.labelFechaInicioCaballo);
             this.Controls.Add(this.dateTimePicker1);
@@ -272,6 +296,8 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label labelFechaInicioCaballo;
         private System.Windows.Forms.Button ActualizaBeneficiario;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
